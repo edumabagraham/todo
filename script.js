44b8b1213efd02fe127cf81dde39a1223b31165d
edumabagraham <edumabagrahamp@gmail.com>
2021-11-28 23:42:48 +0000

    Appending to list successful

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-//Focus on textarea after the page has loaded
+var textArea,item,todoItem,itemText,addLink,element;
 
+//Focus on textarea after the page has loaded
 function setUp(){
-    var textArea = document.getElementById("todo");
+    textArea = document.getElementById("todo");
     textArea.focus();
 }
 
-window.addEventListener('load', setUp, false);
 
-
-//Append to list
-/* This approach is failing mainly because 
-when the enter key is pressed, the value
-of the input is set to an empty string 
-after the keyup event*/ 
-var element;
 function appendItemToList(e) {
-
-  var item = document.createElement("li");
-  var todoItem = document.getElementById("todo").value; // Get todo item entered
-  var itemText = document.createTextNode(todoItem);
-  var itemListPosition = document.getElementsByTagName("ul")[0]; // Get item list
-  
-if(e.keyCode == 13){
-
-      item.appendChild(itemText);
-    itemListPosition.appendChild(item);
-
-    }
- 
+  e.preventDefault();
+  item = document.createElement("li");
+  todoItem = document.getElementById("todo").value; // Get todo item entered
+  itemText = document.createTextNode(todoItem);
+  item.appendChild(itemText);
+  element.appendChild(item);
 }
 
-element = document.getElementById('todo');
-element.addEventListener('keyup', appendItemToList, false);
+element = document.getElementById('itemList');
+addLink = document.getElementById('button');
+addLink.addEventListener('click', appendItemToList, false);
+window.addEventListener('load', setUp, false);
 
 
 //Remove item from the list
